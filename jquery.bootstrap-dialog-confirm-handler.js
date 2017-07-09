509f78c99ec4f8f5667f2d174fa54401edcfe3b4
Vincent Garnier <forxer@gmail.com>
2017-07-09 14:34:18 +0200

    initial commit

---
+++ jquery.bootstrap-dialog-confirm-handler.js
@@ -0,0 +1,32 @@
+
+(function($) {
+
+    $.fn.confirm = function() {
+
+        if (this.length > 1) {
+            this.each(function() {
+                $(this).confirm();
+            });
+
+            return this;
+        }
+
+        return $(this).on('click', function(event) {
+            var $element = $(this);
+
+            if ($element.data('confirmed')) {
+                return true;
+            }
+
+            event.preventDefault();
+
+            BootstrapDialog.confirm($element.data('confirm'), function(yes) {
+                if (yes) {
+                    $element.attr('data-confirmed', 1);
+                    $element.get(0).click();
+                }
+            });
+        });
+    };
+
+})(jQuery);
---
+++ readme.md
@@ -0,0 +1,4 @@
+jQuery Bootstrap Dialog Confirm Handler
+=======================================
+
+jQuery plugin to handle user choice on Bootstrap Dialog Confirm
